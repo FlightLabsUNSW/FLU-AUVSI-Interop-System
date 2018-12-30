51f822f467907cc905989fff1ab92ae4f40efcb5
Marco Alberto <marco.alberto98@gmail.com>
2018-12-31 09:05:11 +1100

    Update and rename InteropTelem_dec18.ps1 to InteropTelemetry.ps1

--- InteropTelemetry.ps1
+++ InteropTelemetry.ps1
@@ -6,9 +6,12 @@ stream into the appropriate format used for the compeititon, and then upload the
 This will continue to run, so long as MAVProxy (on Windows) and the interactive container terminal (on Ubuntu)
 remain open, and the UAV is on and sending data.
 
-##### DO NOT RUN IN POWERSHELL #####
+##### DO NOT RUN IN POWERSHELL
+#### URL is the interop server IP address
+#### Check username and password
+#### Device is any IP address you have access locally to
 
-Note: This script should be run using Ubuntu OS (18.04) with Docker installed onto the device.
+Note: This script should be run using Ubuntu OS (18.04) with Docker installed onto the device, and MAVProxy running.
 
 #>
 
@@ -20,7 +23,3 @@ sudo docker run --net=host --interactive --tty auvsisuas/interop-client:2018.12
 
 # In the interactive container run (check notes below are correct):
 ./tools/interop_cli.py --url http://192.168.1.14:8010 --username testuser --password testpass mavlink --device 192.168.1.17:14550
-
-#### URL is the interop server IP address ####
-#### Check username and password ####
-#### Device is any IP address you have access locally to ####
