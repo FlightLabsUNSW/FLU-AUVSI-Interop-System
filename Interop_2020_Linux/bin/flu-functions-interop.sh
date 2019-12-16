c8596fa0bc3f3aa3ad8608067e26d9bb81a2f79f
Marco Alberto <marco.alberto98@gmail.com>
2019-12-16 17:18:49 +1100

    Update and rename functions.sh to flu-functions-interop.sh

---
+++ Interop_2020_Linux/bin/flu-functions-interop.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+# Functions for the Flight Labs UNSW AUVSI SUAS Interoperability System Interface
+# For use on Ubuntu 18.04
+# Developed by Marco Alberto, December 2019
+
+# Function that unzips, sends files to another folder, and archives the zip folder, preserving it
+function unzipFolder {
+	
+	# Usage: unzipFolder fileArray
+	
+	# Sets the first input to an local variable
+	zippy=$1
+	
+	# Creates a file array with the files in the plane-data folder
+	fileArray=(${fileArray[@]} "$zippy")
+	echo "$zippy"
+	
+	# Unzips the zip folder and sends extracted data to new folder
+	unzip ./$zippy -d $user/bin/plane-data/extracted-data
+	
+	# Moves file to archive folder
+	mv ./$zippy $user/bin/archive
+	echo "$zippy moved to archive"
+
+}
+
+# Function that can be called to check for the uploaded ODLCs (must be run after logging in)
+function checkODLCs {
+
+	# Usage: checkODLCs
+	
+	# Sends a HTTP GET request to the ./api/odlcs address and outputs to a json file
+	curl -b $user/bin/auth.txt -H 'Content-Type: application/json' -o $user/bin/objects-total.json http://$localip:$port/api/odlcs
+
+}
+
+# Function that is used to upload data to the Interop Server
+function uploadData {
+
+	# Usage: uploadData fileExtension fileArray
+	
+	# Set second function input to local variable
+	file=$2
+	
+	# Create a file array with extracted files from the zip folder
+	fileArray=(${fileArray[@]} "$file")
+	echo "$file"
+		
+	# Takes and compares the first function input to check for the correct file extension
+	if [ $1 = "json" ]
+	then
+		echo "The object file $file will be uploaded"
+		
+		# Extracts just the filename without extension
+		fileName=$( basename $file .json)
+		
+		# Moves to the directory with the data in it
+		cd $user/bin/plane-data/extracted-data
+		
+		# Sends a HTTP POST request and output the returned data to a temp json file
+		curl -L -b $user/bin/auth.txt -H 'Content-Type: application/json' -d "@$fileName.json" --output $user/bin/object-temp.json http://$localip:$port/api/odlcs
+		
+		# Read the ID of the ODLC from the temp file of the object just uploaded and print out confirmation
+		id=$(cat $user/bin/object-temp.json | jq '.id')
+		echo $id
+		echo "The ID for the ODLC is $id."
+	
+	elif [ $1 = "jpeg" ]
+	then
+		echo "The image file $file will be uploaded"
+		
+		# Extracts just the filename without extension
+		fileName=$( basename $file .jpeg)
+		
+		# Moves to the directory with data in it
+		cd $user/bin/plane-data/extracted-data
+		
+		# Send a HTTP POST request with binary image data
+		curl -L -b $user/bin/auth.txt -H 'Content-Type: image/jpeg' --data-binary "@$fileName.jpeg" http://$localip:$port/api/odlcs/$id/image
+	
+	elif [ $1 = "png" ] 
+	then	
+		# Functions identically to jpeg if statement except with png file extension
+		echo "The image file $file will be uploaded"
+		fileName=$( basename $file .png)
+		cd $user/bin/plane-data/extracted-data
+		curl -L -b $user/bin/auth.txt -H 'Content-Type: image/png' --data-binary "@$fileName.png" http://$localip:$port/api/odlcs/$id/image
+	else
+		# Print out error messages if the input isn't one of the expected values
+		echo "The file that was unzipped was not a JSON, JPEG or PNG and was not uploaded."
+		echo "Your file will be archived, please check your file extension or name (no spaces)."	
+	fi
+	
+	# Moves the file to the archive, prints confirmation and returns to the plane-data folder
+	mv ./$file $user/bin/archive
+	echo "$file moved to archive"
+	cd $user/bin/plane-data
+
+}
+
+# Function to get the parameters from the login json file (required for login)
+function getParams {
+	
+	# Usage: getParams
+	
+	localip=$(cat $user/bin/mission-params.json | jq -r '.ip')
+	missionID=$(cat $user/bin/mission-params.json | jq '.missionid')
+	port=$(cat $user/bin/mission-params.json | jq '.port')
+	username=$(cat $user/bin/mission-params.json | jq -r '.username')
+	password=$(cat $user/bin/mission-params.json | jq -r '.password')
+
+}
+
+# Function to login to the Interop Server and retrieve mission details
+function interopLogin {
+
+	# Usage: interopLogin
+
+	# Call function to get login parameters
+	getParams
+
+	# Login messages
+	echo "Data was successfully captured by the system, please wait..."
+	echo "Logging on..."
+
+	# Creation of JSON for login information
+	echo {'"username":"'$username'","password":"'$password'"'} > $user/bin/body.json
+
+	# Send HTTP POST request for Interop login and store session variable as a cookie
+	curl -d @$user/bin/body.json -H 'Content-Type: application/json' --cookie-jar $user/bin/auth.txt http://$localip:$port/api/login
+
+	# Send HTTP GET request to Interop Server for mission details
+	curl -L -b $user/bin/auth.txt --output $user/bin/mission-details.json http://$localip:$port/api/missions/$missionID
+
+}
