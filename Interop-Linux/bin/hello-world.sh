5d8188012114d9a66da6317aa3f6284577a09334
Marco <marco.alberto98@gmail.com>
2019-12-24 13:43:56 +1100

    Add required update and modification information

--- Interop-Linux/bin/hello-world.sh
+++ Interop-Linux/bin/hello-world.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 #test - getting there... only this file to fix
+# 1. work out file reference through pwd and make sure sources and .py work
+# 2. install jq for getParams function (in dockerfile)
+# 3. test this bad boy
 
 # Add other files for reference
 source ./bin/flu-functions-interop.sh
